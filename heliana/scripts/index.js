3126e8255aba6ebe94f300ce4f0afe1df6a25f55
Dennis <55185108+MarshaIIs@users.noreply.github.com>
2022-02-17 21:08:07 +0100

    Add files via upload

---
+++ heliana/scripts/index.js
@@ -0,0 +1,28 @@
+Hooks.on("ready", function() {
+  console.log("DDWR! | DDWR! has been successfully initialised and is ready.");
+});
+
+Hooks.on("ready", addExpansionData);
+
+function addExpansionData()
+{
+    const DND5E = CONFIG.DND5E
+
+    DND5E.weaponProperties["hgmhPropLou"] = "Loud";
+	DND5E.weaponProperties["hgmhPropRel"] = "Reload";
+	DND5E.weaponProperties["hgmhPropBal"] = "Balanced";
+	DND5E.spellSchools["hgmhSchBio"] = "Biomancy";
+
+	const tempWeaponIds = DND5E.weaponIds;
+	const newWeaponIds = {
+		haladie: "heliana.hgmh-items.AjIjTGfiCKislDBj",
+		nunchuks: "heliana.hgmh-items.LehYWbpSdHwzmIn7",
+		tetherhook: "heliana.hgmh-items.vLL9siBxTnyJpy0i",
+		blunderbuss: "heliana.hgmh-items.2tUO90YVsBKykxzz",
+		musket: "heliana.hgmh-items.brMUvTjYOaTrfzWh",
+		pistol: "heliana.hgmh-items.ecOhziQfJRwWW6dt",
+		revolver: "heliana.hgmh-items.FCa2F3euhr7vF4EP",
+		rifle: "heliana.hgmh-items.EWQoRth37YCnqddn"
+	}
+	DND5E.weaponIds = Object.assign(tempWeaponIds,newWeaponIds);
+}
